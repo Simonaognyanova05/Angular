538520ea6aa3448ef05c1ff0a79b41bfc89ecca4
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-08-28 15:05:04 +0300

    Add class inheritance

--- Intro to Angular and TypeScript/classes/index.js
+++ Intro to Angular and TypeScript/classes/index.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Car = /** @class */ (function () {
     function Car(brand) {
         this.brand = brand;
@@ -8,19 +23,29 @@ var Car = /** @class */ (function () {
     };
     return Car;
 }());
-var Animal = /** @class */ (function () {
-    function Animal(breed, name, age) {
-        this.breed = breed;
-        this.name = name;
-        this.age = age;
+var DreamCar = /** @class */ (function (_super) {
+    __extends(DreamCar, _super);
+    function DreamCar(brand, model) {
+        var _this = _super.call(this, brand) || this;
+        _this.model = model;
+        return _this;
     }
     ;
-    Animal.prototype.printInfo = function () {
-        console.log("Name of ".concat(this.breed, " is ").concat(this.name, ". It is ").concat(this.age, " years old."));
+    DreamCar.prototype.printDream = function () {
+        console.log(_super.prototype.drive.call(this) + "My Dream is ".concat(this.model, " ").concat(this.brand));
     };
-    return Animal;
-}());
-var car = new Car('Opel');
+    return DreamCar;
+}(Car));
+// class Animal {
+//     constructor(public breed: string, public name: string, public age: number) {
+//     };
+//     printInfo(): void {
+//         console.log(`Name of ${this.breed} is ${this.name}. It is ${this.age} years old.`);
+//     }
+// }
+var car = new Car('Mercedes');
 console.log(car.drive());
-var animal = new Animal('Cat', 'Maxi', 1);
-animal.printInfo();
+var dreamCar = new DreamCar('Opel', 'Mokka');
+dreamCar.printDream();
+// const animal = new Animal('Cat', 'Maxi', 1);
+// animal.printInfo();
--- Intro to Angular and TypeScript/classes/index.ts
+++ Intro to Angular and TypeScript/classes/index.ts
@@ -7,19 +7,32 @@ class Car {
     }
 }
 
-class Animal {
-    constructor(public breed: string, public name: string, public age: number) {
-
+class DreamCar extends Car {
+    constructor(brand: string, protected model: string) {
+        super(brand)
     };
 
-    printInfo(): void {
-        console.log(`Name of ${this.breed} is ${this.name}. It is ${this.age} years old.`);
+    printDream(): void {
+        console.log(super.drive() + ` My Dream is ${this.model} ${this.brand}`);
     }
 }
 
-const car = new Car('Opel');
+// class Animal {
+//     constructor(public breed: string, public name: string, public age: number) {
+
+//     };
+
+//     printInfo(): void {
+//         console.log(`Name of ${this.breed} is ${this.name}. It is ${this.age} years old.`);
+//     }
+// }
+
+const car = new Car('Mercedes');
 console.log(car.drive());
 
-const animal = new Animal('Cat', 'Maxi', 1);
-animal.printInfo();
+const dreamCar = new DreamCar('Opel', 'Mokka');
+dreamCar.printDream();
+
+// const animal = new Animal('Cat', 'Maxi', 1);
+// animal.printInfo();
 
