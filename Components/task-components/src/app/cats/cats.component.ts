10255b40c9892e65175fa8f30aaf59100906524a
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-08-31 12:24:49 +0300

    Add separated html file to components

--- Components/task-components/src/app/cats/cats.component.ts
+++ Components/task-components/src/app/cats/cats.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'cats',
-    template: '<h1>These are my cars</h1>',
+    templateUrl: './cats.component.html',
     styleUrls: ['./cats.component.css']
 })
 
