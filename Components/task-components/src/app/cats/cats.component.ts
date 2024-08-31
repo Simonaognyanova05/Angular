d5ea97861173dad292446da9fe8881ef4076a1bc
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-08-31 12:37:45 +0300

    Add dynamic class to html tag

--- Components/task-components/src/app/cats/cats.component.ts
+++ Components/task-components/src/app/cats/cats.component.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 
-interface Cats{
+interface Cats {
     name: string,
     breed: string,
     age: number,
@@ -12,9 +12,15 @@ interface Cats{
     styleUrls: ['./cats.component.css']
 })
 
-export class Cat{
+export class Cat {
+    isTrue: boolean = false;
+
     cats: Cats[] = [
-        {name: 'Maxi', breed: 'Super cat', age: 1, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSzV5hbZ57XgAywew5lj-PbgvY4-WR2hmQTA&s'},
-        {name: 'Mishi', breed: 'Woman', age: 3, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQre8U4qd9PSAtqTZpGnJW7u9BlUC-RqmaC5Q&s'},
+        { name: 'Maxi', breed: 'Super cat', age: 1, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSzV5hbZ57XgAywew5lj-PbgvY4-WR2hmQTA&s' },
+        { name: 'Mishi', breed: 'Woman', age: 3, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQre8U4qd9PSAtqTZpGnJW7u9BlUC-RqmaC5Q&s' },
     ]
+
+    changeColorHandler(): void {
+        this.isTrue = this.isTrue ? false : true;
+    }
 }
