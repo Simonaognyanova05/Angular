77f66bd8cbf9f8e929d82b1d1e88c5d66b5f267b
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-09-06 17:44:16 +0300

    Export components

--- Workshop-Components/task-components/src/app/app.module.ts
+++ Workshop-Components/task-components/src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    DashboardComponent,
+    RegisterComponent,
+    LoginComponent,
+    CreateComponent,
+    EditComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule
--- Workshop-Components/task/src/app/app.module.ts
+++ Workshop-Components/task/src/app/app.module.ts
@@ -4,11 +4,13 @@ import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CarListComponent } from './car-list/car-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarListComponent
+    CarListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
